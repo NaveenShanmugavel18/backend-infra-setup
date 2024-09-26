f2c6e60aac5ce20e51d99589ca446eff5ad97e5d
Naveen Shanmugavel <naveensv18@gmail.com>
2024-09-26 23:42:21 +0530

    initial project setup

---
+++ products/dist/products/dto/create-product.dto.d.ts
@@ -0,0 +1,4 @@
+export declare class CreateProductDto {
+    productName: string;
+    quantity: number;
+}
---
+++ products/dist/products/product.entity.d.ts
@@ -0,0 +1,5 @@
+export declare class Product {
+    productID: string;
+    productName: string;
+    quantity: number;
+}
